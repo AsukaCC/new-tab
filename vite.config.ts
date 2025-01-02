a114965099595af6d3f13c8c0baa354b4404da7a
JIANGCHENG <plutavian@gmail.com>
2025-01-02 16:35:24 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/new-tab/',
   resolve: {
     alias: {
       '@/': path.resolve(__dirname, './src/**'),
