5b9afaa9b11f370571eff9af91dd4f0ed7264fcc
JIANGCHENG <plutavian@gmail.com>
2025-01-02 16:40:55 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/new-tab/',
+  // base: '/new-tab/', // 发布github page时的路径
   resolve: {
     alias: {
       '@/': path.resolve(__dirname, './src/**'),
